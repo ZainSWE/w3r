aaa3494173217383080986a8c9d4d7f69f58d527
ZainSWE <zainahmad.swe@gmail.com>
2025-06-04 18:58:11 -0400

    initial commit

---
+++ exercises/conditionals/p10_AdmissionEligibilityCheck.c
@@ -0,0 +1,46 @@
+/*
+Write a C program to determine eligibility for admission to a professional course based on the following criteria: Go to the editor
+Eligibility Criteria : Marks in Maths >=65 and Marks in Phy >=55 and Marks in Chem>=50 and Total in all three subject >=190 or Total in Maths and Physics >=140 ------------------------------------- Input the marks obtained in Physics :65 Input the marks obtained in Chemistry :51 Input the marks obtained in Mathematics :72 Total marks of Maths, Physics and Chemistry : 188 Total marks of Maths and Physics : 137 The candidate is not eligible.
+
+Expected Output :
+
+The candidate is not eligible for admission.
+*/
+
+#include <stdio.h>
+
+
+int main() {
+
+    char line[100];
+    double math, physics, chemistry, total, mptotal;
+
+    printf("Enter Math mark: ");
+    fgets(line, sizeof(line), stdin);
+    sscanf(line, "%lf", &math);
+
+    printf("Enter Physics mark: ");
+    fgets(line, sizeof(line), stdin);
+    sscanf(line, "%lf", &physics);
+
+    printf("Enter Chemistry mark: ");
+    fgets(line, sizeof(line), stdin);
+    sscanf(line, "%lf", &chemistry);
+
+    total = math + physics + chemistry;
+    mptotal = math + physics;
+
+    if (math >= 65 && physics >= 55 & chemistry >= 50 && total >= 190) {
+	printf("The candidate is eligible for admission.\n");
+    }
+
+    else if (mptotal >= 140) {
+	printf("The candidate is eligible for admission.\n");
+    }
+
+    else {
+	printf("The candidate is not eligible for admission.\n");
+    }
+
+    return 0;
+}
