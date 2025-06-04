fc529c58568da26b41cc42660c615347e0b4e940
ZainSWE <zainahmad.swe@gmail.com>
2025-06-04 15:25:19 -0400

    initial commit

---
+++ basic/conditionals/p1.c
@@ -0,0 +1,31 @@
+/*
+Write a C program to accept two integers and check whether they are equal or not.
+Test Data : 15 15
+
+Expected Output :
+
+Number1 and Number2 are equal
+*/
+
+#include <stdio.h>
+
+
+int main () {
+
+    char line[100];
+    int num1, num2;
+
+    printf("Enter two integers seperated by a comma: ");
+    fgets(line, sizeof(line), stdin);
+    sscanf(line, "%d,%d", &num1, &num2);
+
+    if (num1 == num2) {
+	printf("Number 1 and Number2 are equal\n");
+    }
+
+    else {
+	printf("Number 1 and Number2 are not equal\n");
+    }
+
+    return 0;
+}
