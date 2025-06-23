65ec7417822029b801b6430315cc202442357d04
Zain Ahmad <zainahmad.swe@gmail.com>
2025-06-23 18:37:07 -0400

    added dynamic allocation for buffer & item + QOL fixes

--- projects/scp.c
+++ projects/scp.c
@@ -1,29 +1,48 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+#define ITEM_LIMIT 255
 
 
 void calculation (double price, int amount);
 
 int main () {
 
-    char buffer[255] = "";
-    char item[255] = "";
+    char *buffer = malloc(sizeof(char) * 255);
+    char *item = malloc(ITEM_LIMIT + 1);
     double price = 0.0;
     int amount = 0;
     int valid = 0;
 
+    if (buffer == NULL || item == NULL) {
+	printf("Error: Failed to allocate memory.\n");
+	exit(1);
+    }
+
     for (int j = 0; valid == 0; j++) {
 	printf("What item would you like to buy?: ");
-	fgets(buffer, sizeof(buffer), stdin);
-	sscanf(buffer, "%s", item);
+	fgets(buffer, 255, stdin);
+	buffer[strcspn(buffer, "\n")] = '\0';
+
+	strncpy(item, buffer, ITEM_LIMIT);
+	item[ITEM_LIMIT] = '\0';
 
 	if ((item[0] >= '0') && (item[0] <= '9')) {
 	    printf("Error: that is not an item, please try again.\n");
 	    valid = 0;
+	    continue;
 	}
 
-	else if (item[0] == '\0') {
+	if (strlen(item) == 0) {
 	    printf("Error: please enter an item and try again.\n");
 	    valid = 0;
+	    continue;
+	}
+
+	else if (strlen(item) > ITEM_LIMIT) {
+	    printf("Error: exceeded maximum character limit, please try again.\n");
 	}
 
 	else {
@@ -65,6 +84,9 @@ int main () {
     }
 
     calculation(price, amount);
+
+    free(buffer);
+    free(item);
 
     return 0;
 }
